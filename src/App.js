5ef4a6a754ccbd79e3eb8a1177929ba575a81b40
Minh Bui <oiconmadn@gmail.com>
2022-03-26 14:24:28 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { ethers, utils } from "ethers";
-import abi from "./contracts/MemeCoin.json";
+import abi from "./contracts/MinhkaCoin.json";
 
 function App() {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
@@ -12,14 +12,20 @@ function App() {
   const [tokenOwnerAddress, setTokenOwnerAddress] = useState(null);
   const [yourWalletAddress, setYourWalletAddress] = useState(null);
   const [error, setError] = useState(null);
+  const [accountBalance, setAccountBalance] = useState(null);
+    
 
-  const contractAddress = 'YOUR CONTRACT ADDRESS HERE';
+  const contractAddress = '0x159b33DA7A151e672a3231447a39B6b39CB01427';
   const contractABI = abi.abi;
 
   const checkIfWalletIsConnected = async () => {
     try {
       if (window.ethereum) {
-
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        const account = accounts[0];
+        setIsWalletConnected(true);
+        setYourWalletAddress(account);
+        console.log("Account Connected: ", account);
       } else {
         setError("Install a MetaMask wallet to get our token.");
         console.log("No Metamask detected");
@@ -32,6 +38,30 @@ function App() {
   const getTokenInfo = async () => {
     try {
       if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        //these functions were inherited via OpenZeppelin
+        let tokenName = await tokenContract.name();
+        let tokenSymbol = await tokenContract.symbol();
+        let tokenOwner = await tokenContract.owner();
+        let tokenSupply = await tokenContract.totalSupply();
+        tokenSupply = utils.formatEther(tokenSupply);
+        
+
+        setTokenName(`${tokenName} 🐲`);
+        setTokenSymbol(tokenSymbol);
+        setTokenTotalSupply(tokenSupply);
+        setTokenOwnerAddress(tokenOwner);
+    
+
+        //checking if the connected account is the token owner,
+        //If that is the case, they will unlock the functionality in our DAPP
+        if (account.toLowerCase() === tokenOwner.toLowerCase()) {
+          setIsTokenOwner(true)
+        }
       }
     } catch (error) {
       console.log(error);
@@ -42,7 +72,18 @@ function App() {
     event.preventDefault();
     try {
       if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+        
+        const txn = await tokenContract.transfer(inputValue.walletAddress, utils.parseEther(inputValue.transferAmount));
+        console.log("Transfering tokens...");
+        await txn.wait();
+        console.log("Tokens Transfered", txn.hash);
 
+        //update the balance of our account
+        updateBalance();
+  
       } else {
         console.log("Ethereum object not found, install Metamask.");
         setError("Install a MetaMask wallet to get our token.");
@@ -56,6 +97,19 @@ function App() {
     event.preventDefault();
     try {
       if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+        
+        const txn = await tokenContract.burn(utils.parseEther(inputValue.burnAmount));
+        console.log("Burning tokens...");
+        await txn.wait();
+        console.log("Tokens burned...", txn.hash);
+
+        let tokenSupply = await tokenContract.totalSupply();
+        tokenSupply = utils.formatEther(tokenSupply)
+        setTokenTotalSupply(tokenSupply);
+
 
       } else {
         console.log("Ethereum object not found, install Metamask.");
@@ -70,6 +124,21 @@ function App() {
     event.preventDefault();
     try {
       if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+        
+        let tokenOwner = await tokenContract.owner();
+        //call the mint() function which comes out of the box with OpenZeppelin 
+        //which actually checks to see if we're the owner of the contract
+        const txn = await tokenContract.mint(tokenOwner, utils.parseEther(inputValue.mintAmount));
+        console.log("Minting tokens...");
+        await txn.wait();
+        console.log("Tokens minted...", txn.hash);
+        
+        let tokenSupply = await tokenContract.totalSupply(); //wait for the new supply
+        tokenSupply = utils.formatEther(tokenSupply) //format it as a readable number using utils.formatEther().
+        setTokenTotalSupply(tokenSupply); //update our token supply.
 
       } else {
         console.log("Ethereum object not found, install Metamask.");
@@ -80,6 +149,29 @@ function App() {
     }
   }
 
+  const updateBalance = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let tokenBalance = await tokenContract.balanceOf(tokenOwnerAddress);
+		    tokenBalance = utils.formatEther(tokenBalance);;
+
+		    
+		    setAccountBalance(tokenBalance);
+
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Please install a MetaMask wallet to use our bank.");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  
   const handleInputChange = (event) => {
     setInputValue(prevFormData => ({ ...prevFormData, [event.target.name]: event.target.value }));
   }
@@ -87,13 +179,14 @@ function App() {
   useEffect(() => {
     checkIfWalletIsConnected();
     getTokenInfo();
+    updateBalance();
   }, [])
 
   return (
     <main className="main-container">
       <h2 className="headline">
-        <span className="headline-gradient">Meme Coin Project</span>
-        <img className="inline p-3 ml-2" src="https://i.imgur.com/5JfHKHU.png" alt="Meme Coin" width="60" height="30" />
+        <span className="headline-gradient">Minhka Coin Project</span>
+        <img className="inline p-3 ml-2" src="https://i.imgur.com/5JfHKHU.png" alt="Minhka Coin" width="60" height="30" />
       </h2>
       <section className="customer-section px-10 pt-5 pb-10">
         {error && <p className="text-2xl text-red-700">{error}</p>}
@@ -102,6 +195,7 @@ function App() {
           <span className="mr-5"><strong>Ticker:</strong>  {tokenSymbol} </span>
           <span className="mr-5"><strong>Total Supply:</strong>  {tokenTotalSupply}</span>
         </div>
+
         <div className="mt-7 mb-9">
           <form className="form-style">
             <input
@@ -125,6 +219,7 @@ function App() {
               onClick={transferToken}>Transfer Tokens</button>
           </form>
         </div>
+
         {isTokenOwner && (
           <section>
             <div className="mt-10 mb-10">
@@ -163,6 +258,7 @@ function App() {
             </div>
           </section>
         )}
+        
         <div className="mt-5">
           <p><span className="font-bold">Contract Address: </span>{contractAddress}</p>
         </div>
@@ -171,6 +267,7 @@ function App() {
         </div>
         <div className="mt-5">
           {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{yourWalletAddress}</p>}
+          {isWalletConnected && <p><span className="font-bold">Balance: </span>{accountBalance} MKCN 🐲</p>}
           <button className="btn-connect" onClick={checkIfWalletIsConnected}>
             {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
           </button>
